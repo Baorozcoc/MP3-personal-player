8d8c9718a0c1f0646537a4e068c9f360dcc4edcf
Baorozcoc <36195164+Baorozcoc@users.noreply.github.com>
2022-09-20 10:58:36 -0500

    No stop

--- mp3player/pages/components/Player.js
+++ mp3player/pages/components/Player.js
@@ -39,7 +39,7 @@ const Player=({listado,Titulo, setTitulo,cancion,setCancion,Autor,setAutor,Categ
             setMaxTiempo(Duracion);
             setPausado(0);
         }   
-        if(tiempo>=maxTiempo){
+        if(tiempo>=cancion.duration-1){
             setTiempo(0);
             return siguienteCancion();
         }
